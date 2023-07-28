fcb8ea3fb20a7659c5c80c058d7763530088f7c9
nohnt9 <nohanbt9@gmail.com>
2023-07-28 21:59:06 +0530

    x1 commit

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const cors=require("cors");
 const path=require("path");
 const app=express();
 app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname,"dist")));
 app.get("/message",(req,res)=>{
 res.send("Twitter is now X");
 });
 app.get("/",(req,res)=>{
 res.sendFile(path.join(__dirname,"dist","index.html"));
+});
+
+app.get("/sendData",(req,res)=>{
+console.log(req);
 });
 
 app.listen(800,()=>{
